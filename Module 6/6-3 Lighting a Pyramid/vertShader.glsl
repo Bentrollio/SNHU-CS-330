63cd7198be53d9ff55b8229b6c2da72eb23a12da
Alex Baires <109702084+Bentrollio@users.noreply.github.com>
2024-02-08 15:54:11 -0500

    Updated uniforms
    Normal is in loc 2
    Color is in loc 3

--- Module 6/6-3 Lighting a Pyramid/vertShader.glsl
+++ Module 6/6-3 Lighting a Pyramid/vertShader.glsl
@@ -1,12 +1,12 @@
 #version 430 core
 layout (location = 0) in vec3 position; // Vertex data from vertexattributepointer 0
-layout (location = 1) in vec4 color; // Color data from vertexattributerpointer 1
+layout (location = 1) in vec4 normal; // Normal data from vertexattributerpointer 1
 layout (location = 2) in vec2 texCoord; // Texture data from vertexattributepointer 2
+layout (location = 3) in vec4 color; // Color data
 
 uniform mat4 mv_matrix;
 uniform mat4 proj_matrix;
-//uniform mat4 model_matrix;
-//uniform mat4 view_matrix;
+uniform mat4 norm_matrix;
 
 out vec4 vertexColor; // sends color to fragment shader
 out vec2 tc; // texture coordinate output to rasterizer for interpolation
